079d18b2cff4ef6cebdd6220cb7e0a8e3082964a
Paolo Ginefra <ginefra.paolo@gmail.com>
2023-03-08 13:39:55 +0100

    feat: added custom logs and slight optimization for computing cumulative weights for the biesed wheel selection

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -3,10 +3,15 @@ import random
 
 
 class Individual:
+    LATEST_ID = 0
+
     def __init__(self, genome=None):
         self.genome = Individual.mutateGenome(
             genome) if genome != None else Individual.getRandomGenome()
         self.fitness = GeneticAlgorithm.getFitness(self)
+
+        self.id = Individual.LATEST_ID
+        Individual.LATEST_ID += 1
 
     def getRandomGenome():
         return random.random()
@@ -19,6 +24,12 @@ class Individual:
 
     def cross_breed(A, B):
         return Individual(genome=A.genome), Individual(genome=B.genome)
+
+    def __eq__(self, other):
+        return self.genome == other.genome
+
+    def __str__(self) -> str:
+        return f'{self.id}: {self.fitness:.2f} - {self.genome}'
 
 
 class GeneticAlgorithm:
@@ -46,11 +57,13 @@ class GeneticAlgorithm:
 
         return candidate2
 
-    def biasedWheelSelection(self):
+    def updateBiesedWheelParameters(self):
         weights = [member.fitness for member in self.population]
-        totalWeights = sum(weights)
-        probabilities = [w / totalWeights for w in weights]
-        return np.random.choice(self.population, 1, probabilities)[0]
+        self.totalWeights = sum(weights)
+        self.probabilities = [w / self.totalWeights for w in weights]
+
+    def biasedWheelSelection(self):
+        return np.random.choice(self.population, 1, self.probabilities)[0]
 
     def chooseParent(self):
         if (random.random() < GeneticAlgorithm.TOURNAMENT_WHEEL_PROB):
@@ -58,7 +71,23 @@ class GeneticAlgorithm:
 
         return self.biasedWheelSelection()
 
+    def updateStatistics(self):
+        self.fittest = self.population[0]
+        self.averageFitness = sum(
+            [member.fitness for member in self.population]) / GeneticAlgorithm.POP_SIZE
+        self.stdFitness = np.std(
+            [member.fitness for member in self.population])
+
+    def logStatistics(self):
+        # print number of generation, fittest, average fitness, standard deviation up to 2 decimal points
+        print(
+            f'Gen:{self.gen} - Fittest: [{self.fittest}] - Average Fitness:{self.averageFitness:.2f} - Standard Deviation:{self.stdFitness:.2f}')
+
     def trainLoop(self):
+        newPopulation = []
+
+        self.updateBiesedWheelParameters()
+
         # Breeding
         for i in range(GeneticAlgorithm.POP_SIZE // 2):
             parent1, parent2 = self.chooseParent(), self.chooseParent()
@@ -69,8 +98,10 @@ class GeneticAlgorithm:
             child1, child2 = Individual.cross_breed(
                 parent1, parent2)
 
-            self.population.append(child1)
-            self.population.append(child2)
+            newPopulation.append(child1)
+            newPopulation.append(child2)
+
+        self.population = self.population + newPopulation
 
         # Sort Population
         self.population = sorted(
@@ -79,7 +110,9 @@ class GeneticAlgorithm:
         # Kill the weak
         self.population = self.population[:GeneticAlgorithm.POP_SIZE]
 
-        self.fittest = self.population[0]
+        self.updateStatistics()
+
+        self.logStatistics()
 
     def __init__(self):
         # Instantiate Population
@@ -91,8 +124,6 @@ class GeneticAlgorithm:
         while self.exitCondition(self.gen):
             # Train Loop
             self.trainLoop()
-
-            print(f'{self.gen} - {self.fittest.fitness}')
 
             self.gen += 1
 
